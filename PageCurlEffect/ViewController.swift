4450a8e38c502d46528f9acab6c71ad2ab12d6f0
Joseph Morris <joseph.morrisIII@hotmail.com>
2019-05-08 14:05:11 -0500

    Contraints are fiixed

--- PageCurlEffect/ViewController.swift
+++ PageCurlEffect/ViewController.swift
@@ -11,38 +11,42 @@ import UIKit
 class ViewController: UIViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
     var pageController: UIPageViewController!
     var controllers = [UIViewController]()
-    var imageNames = ["CupWithBook.png", "openbook.png"]
+    var imageNames: [UIImage] = [
+        UIImage(named: "cup")!,
+        UIImage(named: "open")!
+    ]
+  
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         pageController = UIPageViewController(transitionStyle: .pageCurl, navigationOrientation: .horizontal, options: nil)
-        pageController.isDoubleSided = true
         pageController.dataSource = self
         pageController.delegate = self
         
         addChild(pageController)
         view.addSubview(pageController.view)
-        
-        let views = ["pageController": pageController.view] as [String: AnyObject]
+                let views = ["pageController": pageController.view] as [String: AnyObject]
         view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[pageController]|", options: [], metrics: nil, views: views))
         view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[pageController]|", options: [], metrics: nil, views: views))
         
-        //Append images to book
+        
        
-        
-            //for i in 0 ... 9 {
-                let vc = UIViewController()
-                vc.view.backgroundColor = randomColor()
-                //let imageName = imageNames[i % imageNames.count]
-                let imageView = UIImageView(image: UIImage(named: "open"))
-                vc.view.addSubview(imageView)
-                let subViews = ["imageView": imageView] as [String: AnyObject]
-                vc.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[imageView]|", options: [], metrics: nil, views: subViews))
-                vc.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[imageView]|", options: [], metrics: nil, views: subViews))
-                controllers.append(vc)
-            //}
-        
+      
+        //Append images to book
+        for _ in 1 ... 5 {
+            
+            let vc = UIViewController()
+            vc.view.backgroundColor = randomColor()
+            let imageView = UIImageView(image: UIImage(named: "open"))
+            vc.view.addSubview(imageView)
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            let subViews = ["imageView": imageView] as [String: AnyObject]
+            vc.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[imageView]|", options: [], metrics: nil, views: subViews))
+            vc.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[imageView]|", options: [], metrics: nil, views: subViews))
+            
+            controllers.append(vc)
+        }
         
         pageController.setViewControllers([controllers[0]], direction: .forward, animated: false)
     }
@@ -78,8 +82,6 @@ class ViewController: UIViewController, UIPageViewControllerDataSource, UIPageVi
     func randomColor() -> UIColor {
         return UIColor(red: randomCGFloat(), green: randomCGFloat(), blue: randomCGFloat(), alpha: 1)
     }
-    
-    //let pvc = UIPageViewController(transitionStyle: UIPageViewController.TransitionStyle.pageCurl, navigationOrientation: UIPageViewController.NavigationOrientation.horizontal, options: [UIPageViewController.OptionsKey.spineLocation: UIPageViewController.SpineLocation.min])
-    
- }
+}
+
 
